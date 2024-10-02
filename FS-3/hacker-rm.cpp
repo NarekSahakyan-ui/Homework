c249de62633055d49ebafe936c09fe4e300fb1d6
Narek <nsahakyan42@gmail.com>
2024-10-02 22:10:13 +0400

    FS-3

--- FS-3/hacker-rm.cpp
+++ FS-3/hacker-rm.cpp
@@ -5,48 +5,47 @@
 #include <cstring>
 
 void Delete(const char* path) {
-	int inf = open(path, O_WRONLY);
-	if(inf == -1) {
-		std::cerr << "Error: Cannot open the file" << std::endl;
-		return;
-	}
+    int fd = open(path, O_WRONLY);
+    if (fd == -1) {
+        std::cerr << "Error: Cannot open the file" << std::endl;
+        return;
+    }
 
-	struct stat fileInfo;
-	if(fstat(inf, &fileInfo) == -1) {
-		std::cerr << "Error: Cnnot gen an information about the file" << std::endl;
-		close(inf);
-		return;
-	}
+    struct stat fileInfo;
+    if (fstat(fd, &fileInfo) == -1) {
+        std::cerr << "Error: Cannot get information about the file" << std::endl;
+        close(fd);
+        return;
+    }
 
-	off_t size = fileInfo.st_size;
-	char* buffer = new char[size];
+    off_t size = fileInfo.st_size;
+    if (size > 0) {
+        char* buffer = new char[size];
+        memset(buffer, '\0', size);
 
-	memset(buffer, '\0', size);
+        if (write(fd, buffer, size) != size) {
+            std::cerr << "Error: Failed to write in the file" << std::endl;
+        }
 
-	if(write(inf, buffer, size) != size) {
-		std::cerr << "Error. Failed to write in the file" << std::endl;
-	}
+        delete[] buffer;
+    }
 
-	delete[] buffer;
-	close(inf);
+    close(fd);
 
-	if(unlink(path) == -1) {
-		std::cerr << "Error: Couldnt remove the file" << std::endl;
-	}
-	else {
-		std::cout << "File is successfully deleted" << std::endl;
-	}
-
+    if (unlink(path) == -1) {
+        std::cerr << "Error: Couldn't remove the file" << std::endl;
+    } else {
+        std::cout << "File is successfully deleted" << std::endl;
+    }
 }
-
 
 int main(int argc, char* argv[]) {
-	if(argc != 2) {
-		std::cerr << "THe number of the arguments is wrong." << std::endl;
-		return 1;
-	}
+    if (argc != 2) {
+        std::cerr << "The number of the arguments is wrong." << std::endl;
+        return 1;
+    }
 
-	Delete(argv[1]);
-	return 0;
-
+    Delete(argv[1]);
+    return 0;
 }
+
