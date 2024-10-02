1a20356b15a27c6586693bd4f062f823ad2adb8e
Narek <nsahakyan42@gmail.com>
2024-10-02 12:35:47 +0400

    FS-2

--- FS-2/simple-copy.cpp
+++ FS-2/simple-copy.cpp
@@ -1,43 +1,56 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <cstring>
 
-void copyFile(const std::string& source, const std::string& destination) {
-    std::ifstream src(source, std::ios::binary);
-
-    if (!src) {
+void copyFile(const char* source, const char* destination) {
+    int src_fd = open(source, O_RDONLY);
+    if (src_fd == -1) {
         std::cerr << "Error: Could not open source file: " << source << std::endl;
         return;
     }
 
-    std::ofstream dest(destination, std::ios::binary);
-    if (!dest) {
+    int dest_fd = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (dest_fd == -1) {
         std::cerr << "Error: Could not open destination file: " << destination << std::endl;
+        close(src_fd);
         return;
     }
 
-    dest << src.rdbuf();
+    const std::size_t buffer_size = 256;
+    char buffer[buffer_size];
+    ssize_t bytesRead;
 
-    if (!dest) {
-        std::cerr << "Error: Failed to write to destination file: " << destination << std::endl;
+    while ((bytesRead = read(src_fd, buffer, buffer_size)) > 0) {
+        if (write(dest_fd, buffer, bytesRead) == -1) {
+            std::cerr << "Error: Failed to write to destination file: " << destination << std::endl;
+            close(src_fd);
+            close(dest_fd);
+            return;
+        }
+    }
+
+    if (bytesRead == -1) {
+        std::cerr << "Error: Failed to read from source file: " << source << std::endl;
     } else {
         std::cout << "File copied successfully from " << source << " to " << destination << std::endl;
     }
 
-
-
+    close(src_fd);
+    close(dest_fd);
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        std::cerr << "Error: The number of the argumens is wrong." << std::endl;
+        std::cerr << "Error: The number of the arguments is wrong." << std::endl;
         return 1;
     }
 
-    std::string sourceFile = argv[1];
-    std::string destFile = argv[2];
 
-    copyFile(sourceFile, destFile);
-
+    copyFile(argv[1], argv[2]);
     return 0;
 }
+
+
