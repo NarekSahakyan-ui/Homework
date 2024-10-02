d767087ea0ede5638c203c07b9752b845b7ce8ff
Narek <nsahakyan42@gmail.com>
2024-10-02 12:25:17 +0400

    FS-1

--- FS-1/read-file.cpp
+++ FS-1/read-file.cpp
@@ -1,29 +1,41 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <iostream>
-#include <fstream>
-#include <string>
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Error: File path not provided." << std::endl;
+        return 1;
+    }
 
-        if(argc < 2) {
-                std::cerr << "Error: File path not provided." << std::endl;
-                return 1;
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        std::cerr << "Error: Could not open the file." << std::endl;
+        return 1;
+    }
+
+    const std::size_t buffer_size = 256;
+    char buffer[buffer_size];
+
+    ssize_t bytesRead;
+    while ((bytesRead = read(fd, buffer, buffer_size)) > 0) {
+
+        if (write(STDOUT_FILENO, buffer, bytesRead) == -1) {
+            std::cerr << "Error: Failed to write to stdout." << std::endl;
+            close(fd);
+            return 1;
         }
+    }
 
-        std::ifstream file(argv[1], std::ios::in | std::ios::binary);
-        if(!file) {
-                std::cerr << "Error: Could not open the file." << std::endl;
-                return 1;
-        }
+    if (bytesRead == -1) {
+        std::cerr << "Error: Failed to read the file." << std::endl;
+        close(fd);
+        return 1;
+    }
 
-        std::size_t buffer_size = 256;
-        char buffer[buffer_size];
-
-        while(file.read(buffer, buffer_size) || file.gcount() > 0) {
-                buffer[file.gcount()] = '\0';
-                std::cout << buffer;
-        }
-
-        file.close();
-
-        return 0;
+    close(fd);
+    return 0;
 }
+
